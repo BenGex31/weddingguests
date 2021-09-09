ed048ef62edd2276b999429fc6d31f7ec03a1e31
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-09 19:06:35 +0200

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
 
-export default function BasicTextFields({id, label, variant, defaultValue}) {
-
+export default function BasicTextFields({ id, label, variant, defaultValue, size, color }) {
   return (
-    <form noValidate autoComplete="off">
-      <TextField required id={id} label={label} variant={variant} defaultValue={defaultValue} />
+    <form noValidate autoComplete='off'>
+      <TextField
+        required
+        fullWidth
+        id={id}
+        label={label}
+        variant={variant}
+        defaultValue={defaultValue}
+        size={size}
+        color={color}
+      />
     </form>
   );
 }
