e815b3d8c1de0e087da4dc28d28e04599df275e9
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-10 15:00:20 +0200

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Link from "@material-ui/core/Link";
+import Input from "../components/Input";
 
 const SignUp = () => {
   return (
     <div className='signUpContainerMain'>
-      <div></div>
+      <div>
+        <Input />
+        <Link href='/'>Retour à la page connexion</Link>
+      </div>
     </div>
   );
 };
