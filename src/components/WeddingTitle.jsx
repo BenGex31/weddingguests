0a3294f647df587623358be7c8b20e51ae426110
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-10 13:30:02 +0200

    Create WeddingTitle.jsx

---
+++ src/components/WeddingTitle.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WeddingTitle = () => {
+  return (
+    <h1
+      style={{
+        textAlign: "center",
+        fontFamily: "Allison",
+        fontSize: 55,
+        color: "#76ac34",
+      }}
+    >
+      Mariage
+      <br />
+      Camille et Benjamin
+    </h1>
+  );
+};
+
+export default WeddingTitle;
