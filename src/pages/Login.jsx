d2725403e2bc5cce01c7639594cea3e80aeacbcb
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-10 10:59:18 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Link from "@material-ui/core/Link";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Input from "../components/Input";
+import Button from "../components/Button";
 
 import "./Login.css";
 
@@ -13,6 +15,11 @@ const login = () => {
       <div className='containerForm'>
         <Input id='email' label='Email' variant='outlined' />
         <Input id='password' label='Mot de passe' variant='outlined' />
+        <Button text='Connexion' variant='contained' color='primary' />
+        <div className='containerLink'>
+          <Link href='/forgotpassword'>Mot de passe oublié</Link>
+          <Link href='/signup'>Pas de compte? S'incrire</Link>
+        </div>
       </div>
     </div>
   );
