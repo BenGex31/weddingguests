92c4f22d521fd9454a8eac1b6afa13ec32d21226
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-14 16:31:32 +0200

    Update IconUser.jsx

--- src/components/IconUser.jsx
+++ src/components/IconUser.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import firebaseConfig from "../config/firebase";
+import { Link } from "react-router-dom";
 
 const IconUser = () => {
   return (
     <div>
-      <AccountCircleIcon />
+      <Link to='/profile'>
+        <AccountCircleIcon style={{ color: "black" }} />
+      </Link>
     </div>
   );
 };
