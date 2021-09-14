54c9af675424fa1d13b61b6aaa84a8c2284f58d8
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-14 16:24:57 +0200

    Create IconUser.jsx

---
+++ src/components/IconUser.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import firebaseConfig from "../config/firebase";
+
+const IconUser = () => {
+  return (
+    <div>
+      <AccountCircleIcon />
+    </div>
+  );
+};
+
+export default IconUser;
