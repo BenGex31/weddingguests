fcf6af1776b65a8f8a6c6d8b4449e3da8b1fd8e8
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-14 13:16:05 +0200

    Update AuthNavigation.jsx

--- src/navigation/AuthNavigation.jsx
+++ src/navigation/AuthNavigation.jsx
@@ -3,22 +3,29 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ForgotPassword from "../pages/ForgotPassword";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import Home from "../pages/Home";
+import { AuthProvider } from "../components/Auth";
 
 const AuthNavigation = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Login />
-        </Route>
-        <Route path='/signup'>
-          <SignUp />
-        </Route>
-        <Route path='/forgotpassword'>
-          <ForgotPassword />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <SignUp />
+          </Route>
+          <Route path='/forgotpassword'>
+            <ForgotPassword />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
