ed1a4ff738e8036ec6331c2dd6a07ef9f6793772
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-14 16:24:59 +0200

    Update ForgotPassword.jsx

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import WeddingTitle from "../components/WeddingTitle";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import Link from "@material-ui/core/Link";
 import ReplayIcon from "@material-ui/icons/Replay";
+import firebaseConfig from "../config/firebase";
+
+const auth = firebaseConfig.auth();
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  const sendPasswordReset = () => {
+    const emailResetPassword = email;
+    auth
+      .sendPasswordResetEmail(emailResetPassword)
+      .then(() => {
+        console.log("Password reset email sent!");
+        setEmail("");
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var messageError = error.message;
+        console.log("error reset password" + errorCode, messageError);
+      });
+  };
   return (
     <div
       style={{
@@ -14,6 +33,7 @@ const ForgotPassword = () => {
         justifyContent: "space-between",
         alignItems: "center",
         height: 700,
+        marginBottom: 50,
       }}
     >
       <WeddingTitle />
@@ -22,8 +42,18 @@ const ForgotPassword = () => {
       </div>
       <h2>Réinitialiser mot de passe</h2>
       <div className='containerForm'>
-        <Input id='email' label='Email' variant='outlined' />
-        <Button text='Envoyer' variant='contained' color='primary' />
+        <Input
+          onChange={(text) => setEmail(text.target.value)}
+          id='email'
+          label='Email'
+          variant='outlined'
+        />
+        <Button
+          onClick={sendPasswordReset}
+          text='Envoyer'
+          variant='contained'
+          color='primary'
+        />
         <Link style={{ textAlign: "right" }} underline='none' href='/'>
           Page connexion
         </Link>
