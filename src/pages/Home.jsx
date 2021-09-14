046100b81a97daf7c20330c3cb167c4a3b031815
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-14 13:16:08 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Redirect } from "react-router-dom";
+import { AuthContext } from "../components/Auth";
+import firebaseConfig from "../config/firebase";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { currentUser } = useContext(AuthContext);
+  if (!currentUser) {
+    return <Redirect to='/' />;
+  }
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>This is the dashboard, if you can see this you're logged in.</p>
+      <button onClick={() => firebaseConfig.auth().signOut()}>Sign out</button>
+    </div>
+  );
 };
 
 export default Home;
