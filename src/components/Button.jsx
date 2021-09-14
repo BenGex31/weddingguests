7ea1efc80d154189222a82411e73064e321cc9e2
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-14 13:15:38 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 
-export default function ContainedButtons({ text, variant, color }) {
+export default function ContainedButtons({ text, variant, color, onClick }) {
   return (
-    <Button variant={variant} color={color}>
+    <Button onClick={onClick} variant={variant} color={color}>
       {text}
     </Button>
   );
