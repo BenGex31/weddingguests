345e844f32bff36a72703dcd97ccefdf5d8fc959
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-16 14:44:50 +0200

    Create Formulaire.jsx

---
+++ src/pages/Formulaire.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Formulaire = () => {
+  return (
+    <div>
+      <Header />
+      <div>Formulaire</div>
+    </div>
+  );
+};
+
+export default Formulaire;
