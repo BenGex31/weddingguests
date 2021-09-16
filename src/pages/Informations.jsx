4174cb1efe94873a6910712663b2052bacc15f36
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-16 14:44:59 +0200

    Create Informations.jsx

---
+++ src/pages/Informations.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Informations = () => {
+  return (
+    <div>
+      <Header />
+      <div>Informations</div>
+    </div>
+  );
+};
+
+export default Informations;
