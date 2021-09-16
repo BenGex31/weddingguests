f46fe6638b27b9f5f96fa48a7a533b126cb3ec4a
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-16 14:26:23 +0200

    Update Auth.js
    update styles circularProgress material UI component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,33 +1,42 @@
 import React, { useEffect, useState } from "react";
 import firebaseConfig from "../config/firebase";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 200,
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState(null);
+  const classes = useStyles();
+
   useEffect(() => {
     firebaseConfig.auth().onAuthStateChanged((user) => {
       setCurrentUser(user);
       setLoading(false);
     });
   }, []);
+
   if (loading) {
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: 600,
-          height: 600,
-        }}
-      >
-        <CircularProgress />
+      <div className={classes.root}>
+        <CircularProgress color="primary" size={50} />
       </div>
     );
   }
+
   return (
     <AuthContext.Provider value={{ currentUser }}>
       {children}
