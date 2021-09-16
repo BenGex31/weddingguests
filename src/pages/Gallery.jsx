aa39b40feaab069e32d22560bcfbc9afe5909eee
Benjamin Desole <benjamin.desole@gmail.com>
2021-09-16 14:44:54 +0200

    Create Gallery.jsx

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Gallery = () => {
+  return (
+    <div>
+      <Header />
+      <div>Gallerie photos</div>
+    </div>
+  );
+};
+
+export default Gallery;
