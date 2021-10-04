9e656c1a05a49a96cfdf0f5086f945fd7b7838a4
Benjamin Desole <benjamin.desole@gmail.com>
2021-10-04 16:41:13 +0200

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import firebaseConfig from "../config/firebase";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
-import CustomizedSnackbars from "./SnackBar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,14 +20,12 @@ export const AuthContext = React.createContext();
 export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState(null);
-  const [openSnackBar, setOpenSnackBar] = React.useState(false);
   const classes = useStyles();
 
   useEffect(() => {
     firebaseConfig.auth().onAuthStateChanged((user) => {
       setCurrentUser(user);
       setLoading(false);
-      setOpenSnackBar(true);
     });
   }, []);
 
@@ -37,25 +34,6 @@ export const AuthProvider = ({ children }) => {
       <div className={classes.root}>
         <CircularProgress color='primary' size={100} />
       </div>
-    );
-  }
-
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpenSnackBar(false);
-  };
-
-  if (openSnackBar) {
-    return (
-      <CustomizedSnackbars
-        open={openSnackBar}
-        handleClose={handleClose}
-        severity={currentUser ? "success" : "info"}
-        text={currentUser ? "Connexion réussie !" : "Vous êtes déconnecté !"}
-      />
     );
   }
 
