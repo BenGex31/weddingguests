951d2e7b2f61b9530e74d3a6678f8d8a7361d49a
Benjamin Desole <benjamin.desole@gmail.com>
2021-10-04 16:41:43 +0200

    Update ForgotPassword.jsx

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -5,11 +5,28 @@ import Button from "../components/Button";
 import Link from "@material-ui/core/Link";
 import ReplayIcon from "@material-ui/icons/Replay";
 import firebaseConfig from "../config/firebase";
+import CustomizedSnackbars from "../components/CustomizedSnackbars";
 
 const auth = firebaseConfig.auth();
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
+  const [isValidEmail, setIsvalidEmail] = useState(false);
+  const [messageEmailError, setMessageEmailError] = useState("");
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  const handleChangeEmail = (event) => {
+    if (emailRegex.test(event.target.value)) {
+      setIsvalidEmail(true);
+      setMessageEmailError("Votre adresse mail est correcte !");
+    } else {
+      setIsvalidEmail(false);
+      setMessageEmailError("Entrez une adresse mail valide !");
+    }
+    setEmail(event.target.value);
+  };
 
   const sendPasswordReset = () => {
     const emailResetPassword = email;
@@ -19,11 +36,27 @@ const ForgotPassword = () => {
         console.log("Password reset email sent!");
         setEmail("");
       })
+      .then(() => {
+        setOpenSnackBar(true);
+      })
+      .then(() => {
+        setIsvalidEmail(false);
+      })
+      .then(() => {
+        setMessageEmailError("");
+      })
       .catch((error) => {
         var errorCode = error.code;
         var messageError = error.message;
         console.log("error reset password" + errorCode, messageError);
       });
+  };
+
+  const handleCloseSnackBar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackBar(false);
   };
   return (
     <div
@@ -43,21 +76,38 @@ const ForgotPassword = () => {
       <h2>Réinitialiser mot de passe</h2>
       <div className='containerForm'>
         <Input
-          onChange={(text) => setEmail(text.target.value)}
+          onChange={handleChangeEmail}
           id='email'
           label='Email'
           variant='outlined'
+          value={email}
         />
+        {isValidEmail ? (
+          <span style={{ color: "green", fontSize: 12 }}>
+            {messageEmailError}
+          </span>
+        ) : (
+          <span style={{ color: "red", fontSize: 12 }}>
+            {messageEmailError}
+          </span>
+        )}
         <Button
           onClick={sendPasswordReset}
           text='Envoyer'
           variant='contained'
           color='primary'
+          disabled={isValidEmail ? false : true}
         />
         <Link style={{ textAlign: "right" }} underline='none' href='/'>
           Page connexion
         </Link>
       </div>
+      <CustomizedSnackbars
+        open={openSnackBar}
+        handleClose={handleCloseSnackBar}
+        text='Un mail vous a été envoyé'
+        severity='success'
+      />
     </div>
   );
 };
