3c64c580348d41494834f6253b2d943bfcd9141e
Benjamin Desole <benjamin.desole@gmail.com>
2021-10-16 00:12:29 +0200

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,11 +5,14 @@ import Input from "../components/Input";
 import Button from "../components/Button";
 import ErrorMessage from "../components/ErrorMessage";
 import InputAdornments from "../components/InputAdornmets";
-import "./SignUp.css";
 import WeddingTitle from "../components/WeddingTitle";
 import PersonIcon from "@material-ui/icons/Person";
 import weddingHall1 from "../assets/beyssac1.jpeg";
 import firebaseConfig from "../config/firebase";
+import CssBaseline from "@mui/material/CssBaseline";
+import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
+import Container from "@material-ui/core/Container";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -95,94 +98,121 @@ const SignUp = () => {
   }
 
   return (
-    <div className='signUpContainerMain'>
-      <div className='signUpTitleImg'>
-        <img className='imgTitle' src={weddingHall1} alt='beyssac' />
-        <WeddingTitle />
-      </div>
-      <div className='containerIcon'>
-        <PersonIcon style={{ fontSize: 40, color: "white" }} />
-      </div>
-      <h2>Créer un compte</h2>
-      <div className='signUpForm'>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <Input
-            id='firstname'
-            label='Prénom'
-            variant='outlined'
-            onChange={(text) => setFirstName(text.target.value)}
+    <Container
+      style={{ marginTop: 20, marginBottom: 20, height: 550 }}
+      maxWidth='lg'
+    >
+      <CssBaseline />
+      <Stack
+        sx={{ height: "100%" }}
+        spacing={{ xs: 1, sm: 2, md: 4 }}
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent='space-around'
+        alignItems='center'
+      >
+        <Stack alignItems='center'>
+          <WeddingTitle />
+          <img
+            width={320}
+            height={213.333333333333333}
+            style={{ borderRadius: 20 }}
+            src={weddingHall1}
+            alt='beyssac'
           />
-          <Input
-            id='lastname'
-            label='Nom de famille'
-            variant='outlined'
-            onChange={(text) => setLastName(text.target.value)}
-          />
-        </div>
-        <Input
-          id='email'
-          label='Email'
-          variant='outlined'
-          onChange={handleChangeEmail}
-        />
-        {isValidEmail ? (
-          <span style={{ color: "green", fontSize: 12 }}>
-            {messageEmailError}
-          </span>
-        ) : (
-          <span style={{ color: "red", fontSize: 12 }}>
-            {messageEmailError}
-          </span>
-        )}
-        <InputAdornments
-          id='password'
-          type={passwordVisibility ? "text" : "password"}
-          value={password}
-          onChange={handleChangePassWord("password")}
-          onClick={handleClickShowPassword}
-          onMouseDown={handleMouseDownPassword}
-          visibility
-        />
-        {isValidPassword ? (
-          <span style={{ color: "green", fontSize: 12 }}>
-            {" "}
-            {errorMessagePassword}
-          </span>
-        ) : (
-          <span style={{ color: "red", fontSize: 12 }}>
-            {" "}
-            {errorMessagePassword}
-          </span>
-        )}
-        {/*<Input
-          id='photo'
-          variant='standard'
-          type='file'
-          value={selectedFile}
-          onChange={(file) => setSelectedFile(file.target.value)}
-        />*/}
-        {signupError ? (
-          <ErrorMessage error={signupError} visible={true} />
-        ) : null}
-        <Button
-          text="S'incrire"
-          variant='contained'
-          color='primary'
-          onClick={onHandleSignup}
-          disabled={
-            isValidEmail &&
-            isValidPassword &&
-            firstname !== "" &&
-            lastname !== ""
-              ? false
-              : true
-          }
-        />
-        <Link style={{ textAlign: "right", fontSize: 15 }} href='/'>
-          Page connexion
-        </Link>
-      </div>
-    </div>
+        </Stack>
+        <Stack>
+          <Stack alignItems='center' mt={2}>
+            <Stack>
+              <PersonIcon
+                style={{
+                  fontSize: 60,
+                  padding: 15,
+                  borderRadius: 50,
+                  color: "white",
+                  backgroundColor: "black",
+                }}
+              />
+            </Stack>
+            <h2>Créer un compte</h2>
+          </Stack>
+          <Stack spacing={1} width={{ xs: 310, sm: 420 }}>
+            <Stack
+              spacing={1}
+              direction={{ xs: "column", sm: "row" }}
+              justifyContent='space-between'
+            >
+              <Input
+                id='firstname'
+                label='Prénom'
+                variant='outlined'
+                onChange={(text) => setFirstName(text.target.value)}
+              />
+              <Input
+                id='lastname'
+                label='Nom de famille'
+                variant='outlined'
+                onChange={(text) => setLastName(text.target.value)}
+              />
+            </Stack>
+            <Input
+              id='email'
+              label='Email'
+              variant='outlined'
+              onChange={handleChangeEmail}
+            />
+            {isValidEmail ? (
+              <span style={{ color: "green", fontSize: 12 }}>
+                {messageEmailError}
+              </span>
+            ) : (
+              <span style={{ color: "red", fontSize: 12 }}>
+                {messageEmailError}
+              </span>
+            )}
+            <InputAdornments
+              id='password'
+              type={passwordVisibility ? "text" : "password"}
+              value={password}
+              onChange={handleChangePassWord("password")}
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              visibility
+            />
+            {isValidPassword ? (
+              <span style={{ color: "green", fontSize: 12 }}>
+                {" "}
+                {errorMessagePassword}
+              </span>
+            ) : (
+              <span style={{ color: "red", fontSize: 12 }}>
+                {" "}
+                {errorMessagePassword}
+              </span>
+            )}
+            {signupError ? (
+              <ErrorMessage error={signupError} visible={true} />
+            ) : null}
+            <Button
+              text="S'incrire"
+              variant='contained'
+              color='primary'
+              onClick={onHandleSignup}
+              disabled={
+                isValidEmail &&
+                isValidPassword &&
+                firstname !== "" &&
+                lastname !== ""
+                  ? false
+                  : true
+              }
+            />
+            <Link style={{ textAlign: "right", fontSize: 15 }} href='/'>
+              Page connexion
+            </Link>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
 
