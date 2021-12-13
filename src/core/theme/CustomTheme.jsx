12585657c29862c0980ada0d321ccf1b7f7b0691
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-13 22:51:16 +0100

    Défine Mui and Custom theme

---
+++ src/core/theme/CustomTheme.jsx
@@ -0,0 +1,17 @@
+export const oswaldRegular = {
+  fontFamily: "oswald,sans-serif",
+  fontWeight: 400,
+  fontStyle: "normal",
+};
+
+export const oswaldBold = {
+  fontFamily: "oswald,sans-serif",
+  fontWeight: 700,
+  fontStyle: "normal",
+};
+
+export const alexBrushRegular = {
+  fontFamily: "alex-brush, sans-serif",
+  fontWeight: 400,
+  fontStyle: "normal",
+};
---
+++ src/core/theme/MuiTheme.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#B88961",
+    },
+    secondary: {
+      main: "#888888",
+      light: "#F4F4F4",
+    },
+  },
+});
+
+export default theme;
