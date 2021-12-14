ac72776c1354e69ca9115b8a07769dc79ab0a74f
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-14 02:28:07 +0100

    Update Informations.jsx

--- src/pages/Informations.jsx
+++ src/pages/Informations.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "../components/Header";
 
 const Informations = () => {
   return (
     <div>
-      <Header />
       <div>Informations</div>
     </div>
   );
