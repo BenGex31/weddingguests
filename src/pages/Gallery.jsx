58e7afe23c9f2baa4df35671584c0360c50861d0
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-14 02:27:45 +0100

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "../components/Header";
 
 const Gallery = () => {
   return (
     <div>
-      <Header />
       <div>Gallerie photos</div>
     </div>
   );
