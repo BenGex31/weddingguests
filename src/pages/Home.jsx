4ed8b64293e0bde4d25c98a024f78c6f1bf06c8f
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-14 02:28:01 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../components/Auth";
-import "./Home.css";
-import Header from "../components/Header";
 
 const Home = () => {
   const { currentUser } = useContext(AuthContext);
@@ -11,17 +9,7 @@ const Home = () => {
     return <Redirect to='/' />;
   }
 
-  return (
-    <>
-      <div>
-        <Header />
-        <div className='homeContainer'>
-          <h1>Bienvenue</h1>
-          <p>Sur le site du Mariage de Camille et Benjamin</p>
-        </div>
-      </div>
-    </>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
