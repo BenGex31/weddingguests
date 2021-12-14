99a437954b0e58642415abbd47ebffc53f85f8cf
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-14 02:27:23 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,15 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import IconLogout from "./IconLogout";
-import IconUser from "./IconUser";
-import Avatar from "@mui/material/Avatar";
-import weddingLogo from "../assets/weddingLogo.png";
-import "./Header.css";
 import { AuthContext } from "./Auth";
+import weddingOfficial from "../assets/weddingOfficial.svg";
+import Grid from "@mui/material/Grid";
 
 const Header = () => {
   const { currentUser } = useContext(AuthContext);
   console.log(currentUser);
   return (
-    <div className='headerContainer'>
-      <img
-        style={{ width: 100, height: 100 }}
-        src={weddingLogo}
-        alt='logo mariage'
-      />
-      <nav className='headerNav'>
-        <Link to='/home'>Acceuil</Link>
-        <Link to='/galerie'>Galerie Photos</Link>
-        <Link to='/informations'>Informations</Link>
-        <Link to='/formulaire'>Formulaire</Link>
-      </nav>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column-reverse",
-          justifyContent: "space-between",
-          alignItems: "center",
-          height: 60,
-        }}
-      >
-        <div className='headerIcons'>
-          {currentUser.photoURL !== null ? (
-            <Avatar alt={currentUser.displayName} src={currentUser.photoURL} />
-          ) : (
-            <IconUser />
-          )}
-          <IconLogout />
-        </div>
-        <div>
-          <span style={{ fontSize: 12, fontWeight: "bold" }}>
-            {currentUser.displayName}
-          </span>
-        </div>
-      </div>
-    </div>
+    <Grid container>
+      <img src={weddingOfficial} alt='wedding-official' />
+    </Grid>
   );
 };
 
