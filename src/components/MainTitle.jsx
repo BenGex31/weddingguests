90ed798eb986c1908f9e42275d9afacb87b474ae
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-15 23:05:06 +0100

    Update MainTitle.jsx

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -2,23 +2,20 @@ import React from "react";
 import { Grid, Typography } from "@mui/material";
 import theme from "../core/theme/MuiTheme";
 import { alexBrushRegular as alexBrush } from "../core/theme/CustomTheme";
-import makeStyles from "@mui/styles/makeStyles";
-
-const useStyles = makeStyles(() => ({
-  titleStyle: {
-    fontSize: 66,
-    fontFamily: alexBrush.fontFamily,
-    fontWeight: alexBrush.fontWeight,
-    fontStyle: alexBrush.fontStyle,
-    color: theme.palette.primary.main,
-  },
-}));
 
 const MainTitle = ({ title }) => {
-  const classes = useStyles();
   return (
     <Grid container justifyContent='center'>
-      <Typography className={classes.titleStyle} variant='h1'>
+      <Typography
+        sx={{
+          fontSize: 66,
+          fontFamily: alexBrush.fontFamily,
+          fontWeight: alexBrush.fontWeight,
+          fontStyle: alexBrush.fontStyle,
+          color: theme.palette.primary.main,
+        }}
+        variant='h1'
+      >
         {title}
       </Typography>
     </Grid>
