70d05dcd18569614a0c2d19b51b6de7246ece39b
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-19 03:05:43 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./Auth";
-import weddingOfficial from "../assets/weddingOfficial.svg";
-import Grid from "@mui/material/Grid";
+import weddingOfficial from "../assets/weddingOfficial.jpeg";
+import { Box } from "@mui/material";
 
 const Header = () => {
   const { currentUser } = useContext(AuthContext);
   console.log(currentUser);
   return (
-    <Grid container>
-      <img src={weddingOfficial} alt='wedding-official' />
-    </Grid>
+    <Box
+      height={801}
+      width={1024}
+      sx={{
+        backgroundImage: `url(${weddingOfficial})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    />
+    /* <img
+      src={weddingOfficial}
+      className='img-fluid'
+      width={1319}
+      alt='official'
+    />*/
   );
 };
 
