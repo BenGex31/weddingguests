e0116d6e5e5c3b3a255dc7ced20b9cd86ed384e6
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-19 03:06:15 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import { Redirect } from "react-router-dom";
 import { AuthContext } from "../components/Auth";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
-import MainTitle from "../components/MainTitle";
-import { Grid, Stack, Typography, Divider } from "@mui/material";
+import { Stack, Typography, Divider } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { oswaldRegular as OswaldR } from "../core/theme/CustomTheme";
+import { alexBrushRegular as alexBrush } from "../core/theme/CustomTheme";
 import theme from "../core/theme/MuiTheme";
 import Cards from "../components/Card";
 import Countdown from "react-countdown";
@@ -20,13 +20,22 @@ const Home = () => {
   }
 
   return (
-    <Grid>
-      <header>
-        <Header />
-        <NavBar />
-      </header>
-      <MainTitle title='Nous avons choisi une date' />
-      <Stack mt={1}>
+    <Stack display={"flex"} alignItems={"center"}>
+      <Header />
+      <NavBar />
+      <Stack justifyContent={"center"} alignItems={"center"} spacing={5}>
+        <Typography
+          sx={{
+            fontSize: { md: 66, xs: 40 },
+            fontFamily: alexBrush.fontFamily,
+            fontWeight: alexBrush.fontWeight,
+            fontStyle: alexBrush.fontStyle,
+            color: theme.palette.primary.main,
+          }}
+          variant='h1'
+        >
+          Nous avons choisi une date
+        </Typography>
         <Typography
           sx={{
             fontSize: 25,
@@ -35,72 +44,104 @@ const Home = () => {
             fontStyle: OswaldR.fontStyle,
             color: theme.palette.secondary.main,
           }}
-          align='center'
           variant='h2'
         >
           Et nous comptons les jours!
         </Typography>
-      </Stack>
-      <Stack alignItems='center' mt={4.5}>
-        <Divider sx={{ width: 617 }} orientation='horizontal'>
-          <FavoriteIcon
-            sx={{ color: theme.palette.primary.main, width: 36, height: 31.5 }}
+        <Stack justifyContent={"center"} alignItems='center'>
+          <Divider sx={{ width: 617 }} orientation='horizontal'>
+            <FavoriteIcon
+              sx={{
+                color: theme.palette.primary.main,
+                width: 36,
+                height: 31.5,
+              }}
+            />
+          </Divider>
+        </Stack>
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          justifyContent={"center"}
+          alignItems={"center"}
+          spacing={6}
+        >
+          <Cards
+            number={
+              <Countdown
+                date={Date.now() + (new Date(2022, 4, 26) - new Date())}
+                renderer={({ days }) => {
+                  return <span>{days}</span>;
+                }}
+              />
+            }
+            time={"Jours"}
           />
-        </Divider>
+          <Cards
+            number={
+              <Countdown
+                date={Date.now() + (new Date(2022, 4, 26) - new Date())}
+                renderer={({ hours }) => {
+                  return <span>{hours}</span>;
+                }}
+              />
+            }
+            time={"Heures"}
+          />
+          <Cards
+            number={
+              <Countdown
+                date={Date.now() + (new Date(2022, 4, 26) - new Date())}
+                renderer={({ minutes }) => {
+                  return <span>{minutes}</span>;
+                }}
+              />
+            }
+            time={"minutes"}
+          />
+          <Cards
+            number={
+              <Countdown
+                date={Date.now() + (new Date(2022, 5, 25) - new Date())}
+                renderer={({ seconds }) => {
+                  return <span>{seconds}</span>;
+                }}
+              />
+            }
+            time={"Secondes"}
+          />
+        </Stack>
+        <Stack>
+          <Typography
+            sx={{
+              fontSize: 38,
+              fontFamily: alexBrush.fontFamily,
+              fontWeight: alexBrush.fontWeight,
+              fontStyle: alexBrush.fontStyle,
+              color: theme.palette.primary.main,
+            }}
+            align='center'
+            variant='h3'
+          >
+            25 mai 2022
+          </Typography>
+        </Stack>
+        <Stack>
+          <Typography
+            sx={{
+              fontSize: 90,
+              fontFamily: alexBrush.fontFamily,
+              fontWeight: alexBrush.fontWeight,
+              fontStyle: alexBrush.fontStyle,
+              color: theme.palette.primary.main,
+            }}
+            align='center'
+            variant='h1'
+          >
+            L'heureux couple
+          </Typography>
+        </Stack>
       </Stack>
-      <Stack
-        mt={6}
-        mb={6}
-        direction={"row"}
-        justifyContent={"center"}
-        spacing={4}
-      >
-        <Cards
-          number={
-            <Countdown
-              date={Date.now() + (new Date(2022, 4, 26) - new Date())}
-              renderer={({ days }) => {
-                return <span>{days}</span>;
-              }}
-            />
-          }
-          time={"Jours"}
-        />
-        <Cards
-          number={
-            <Countdown
-              date={Date.now() + (new Date(2022, 4, 26) - new Date())}
-              renderer={({ hours }) => {
-                return <span>{hours}</span>;
-              }}
-            />
-          }
-          time={"Heures"}
-        />
-        <Cards
-          number={
-            <Countdown
-              date={Date.now() + (new Date(2022, 4, 26) - new Date())}
-              renderer={({ minutes }) => {
-                return <span>{minutes}</span>;
-              }}
-            />
-          }
-          time={"minutes"}
-        />
-        <Cards
-          number={
-            <Countdown
-              date={Date.now() + (new Date(2022, 5, 25) - new Date())}
-              renderer={({ seconds }) => {
-                return <span>{seconds}</span>;
-              }}
-            />
-          }
-          time={"Secondes"}
-        />
-      </Stack>
-    </Grid>
+    </Stack>
   );
 };
 
