df839ac3adff068bb88412ad51001d6a18c11489
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-23 23:13:03 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "./Auth";
 import weddingOfficial from "../assets/weddingOfficial.jpeg";
 import { Box } from "@mui/material";
 
 const Header = () => {
   const { currentUser } = useContext(AuthContext);
+  const [mobileView, setMobileView] = useState(false);
   console.log(currentUser);
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setMobileView(true)
+        : setMobileView(false);
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
   return (
-    <Box
-      height={801}
-      width={1300}
-      sx={{
-        backgroundImage: `url(${weddingOfficial})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    />
-    /* <img
-      src={weddingOfficial}
-      className='img-fluid'
-      width={1319}
-      alt='official'
-    />*/
+    <>
+      {!mobileView ? (
+        <Box
+          height={801}
+          width={1300}
+          sx={{
+            backgroundImage: `url(${weddingOfficial})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        />
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
