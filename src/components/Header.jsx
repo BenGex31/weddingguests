87195b612f59e1b5954629fd7b1545dd5f30f29d
Benjamin Desole <benjamin.desole@gmail.com>
2021-12-30 12:08:37 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "./Auth";
 import theme from "../core/theme/MuiTheme";
 import weddingOfficial from "../assets/weddingOfficial.jpeg";
-import { Box } from "@mui/material";
+import { Box, Container, Divider, Typography } from "@mui/material";
+import { oswaldExtraLight as oswaldExtra } from "../core/theme/CustomTheme";
+import { alexBrushRegular as alexBrush } from "../core/theme/CustomTheme";
 
 const Header = () => {
   const { currentUser } = useContext(AuthContext);
@@ -27,18 +29,62 @@ const Header = () => {
   return (
     <>
       {!mobileView ? (
-        <Box
-          height={801}
-          width={1300}
+        <Container
           sx={{
-            backgroundImage: `url(${weddingOfficial})`,
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            boxShadow: "0px 0px 20px" + theme.palette.secondary.main,
-            borderRadius: 5,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
-        />
+          maxWidth='xl'
+        >
+          <Box
+            height={801}
+            width={1200}
+            sx={{
+              backgroundImage: `url(${weddingOfficial})`,
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              boxShadow: "0px 0px 20px" + theme.palette.secondary.main,
+              borderRadius: 5,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Divider
+              sx={{ width: 656, backgroundColor: "#FFFFFF" }}
+              orientation='horizontal'
+            />
+            <Typography
+              sx={{
+                fontSize: 30,
+                fontFamily: oswaldExtra.fontFamily,
+                fontWeight: oswaldExtra.fontWeight,
+                fontStyle: oswaldExtra.fontStyle,
+                color: "#FFFFFF",
+              }}
+            >
+              Le mariage de
+            </Typography>
+            <Typography
+              variant='h1'
+              sx={{
+                fontFamily: alexBrush.fontFamily,
+                fontWeight: alexBrush.fontWeight,
+                fontStyle: alexBrush.fontStyle,
+                color: "#FFFFFF",
+              }}
+            >
+              Camille & Benjamin
+            </Typography>
+            <Divider
+              sx={{ width: 656, backgroundColor: "#FFFFFF" }}
+              orientation='horizontal'
+            />
+          </Box>
+        </Container>
       ) : (
         <></>
       )}
