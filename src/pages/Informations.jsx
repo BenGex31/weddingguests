f2d6a2038716eaf83d9b4973fef14cb892508dcf
Benjamin Desole <benjamin.desole@gmail.com>
2022-01-05 02:23:19 +0100

    Update Informations.jsx
    remove unused code

--- src/pages/Informations.jsx
+++ src/pages/Informations.jsx
@@ -7,27 +7,6 @@ import MainTitle from "../components/MainTitle";
 import Map from "../components/Map";
 
 const Informations = () => {
-  /*const configHotel = {
-    method: "get",
-    mode: "no-cors",
-    url:
-      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=hotel&key=" +
-      reactMapConfig.REACT_APP_MAP_API_KEY,
-    headers: {
-      "Access-Control-Allow-Origin": " https://maps.googleapis.com/maps/api/",
-    },
-  };
-
-  useEffect(() => {
-    axios(configHotel)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  });*/
-
   const { currentUser } = useContext(AuthContext);
 
   if (!currentUser) {
@@ -35,7 +14,7 @@ const Informations = () => {
   }
 
   return (
-    <Container component='main' maxWidth='lg'>
+    <Container component='main' maxWidth='xl'>
       <header>
         <Header />
       </header>
