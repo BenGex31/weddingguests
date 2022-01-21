f6431c6487d21df7ef297931f50e74cfd356dd61
Benjamin Desole <benjamin.desole@gmail.com>
2022-01-21 01:31:12 +0100

    Update Formulaire.jsx

--- src/pages/Formulaire.jsx
+++ src/pages/Formulaire.jsx
@@ -17,9 +17,24 @@ import theme from "../core/theme/MuiTheme";
 import { oswaldLight as oswaldFontLight } from "../core/theme/CustomTheme";
 import { AddReaction } from "@mui/icons-material";
 import firebaseConfig from "../config/firebase";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => ({
+  formChild: {
+    backgroundColor: theme.palette.secondary.light,
+    borderRadius: 10,
+    padding: 5,
+    boxShadow: "0px 0px 20px" + theme.palette.secondary.main,
+    margin: 10,
+  },
+  subContainer: {
+    marginBottom: 30,
+  },
+}));
 
 const Formulaire = () => {
-  const [responsePresence, setResponsePresence] = useState("null");
+  const classes = useStyles();
+  const [responsePresence, setResponsePresence] = useState("");
   const [isAllergy, setIsAllergy] = useState("");
   const [responseAllergy, setResponseAllergy] = useState("");
   const [engagementCeremony, setEngagementCeremony] = useState(false);
@@ -30,13 +45,13 @@ const Formulaire = () => {
 
   const onLastNameChange = (event, index) => {
     let array = [...childrenList];
-    array[index].lastName = event.target.value;
+    array[index].lastName = event.target.value.trimEnd();
     setChildrenList(array);
   };
 
   const onFirstNameChange = (event, index) => {
     let array = [...childrenList];
-    array[index].firstName = event.target.value;
+    array[index].firstName = event.target.value.trimEnd();
     setChildrenList(array);
   };
 
@@ -48,6 +63,25 @@ const Formulaire = () => {
         : isNaN(parseInt(event.target.value))
         ? ""
         : parseInt(event.target.value);
+    setChildrenList(array);
+  };
+
+  const onChildIsAllergyChange = (event, index) => {
+    let array = [...childrenList];
+    array[index].isAllergy = event.target.value;
+    setChildrenList(array);
+  };
+
+  const onChildAllergiesChange = (event, index) => {
+    let array = [...childrenList];
+    array[index].allergies = event.target.value.trimEnd();
+    setChildrenList(array);
+  };
+
+  const onDeleteChildClick = (item) => {
+    let array = [...childrenList];
+    let indexToSearch = array.findIndex((child) => child.id === item.id);
+    array.splice(indexToSearch, 1);
     setChildrenList(array);
   };
 
@@ -79,120 +113,163 @@ const Formulaire = () => {
             autoComplete='off'
           >
             <Grid
+              className={classes.subContainer}
               container
               alignItems={"center"}
               justifyContent={"space-around"}
             >
-              <TextField
-                id='presence'
-                select
-                variant='standard'
-                label='Votre présence ?'
-                sx={{
-                  fontFamily: oswaldFontLight.fontFamily,
-                  fontWeight: oswaldFontLight.fontWeight,
-                  fontStyle: oswaldFontLight.fontStyle,
-                }}
-                value={responsePresence}
-                onChange={(event) => setResponsePresence(event.target.value)}
-              >
-                <MenuItem value={"Ne sais pas"}>Ne sais pas</MenuItem>
-                <MenuItem value={"oui"}>Oui</MenuItem>
-                <MenuItem value={"non"}>Non</MenuItem>
-              </TextField>
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={engagementCeremony}
-                      onChange={(event) =>
-                        setEngagementCeremony(event.target.checked)
-                      }
-                    />
-                  }
-                  label='A la cérémonie d’engagement'
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={wineReception}
-                      onChange={(event) =>
-                        setWineReception(event.target.checked)
-                      }
-                    />
-                  }
-                  label='Au vin d’honneur et Apéritif'
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={meal}
-                      onChange={(event) => setMeal(event.target.checked)}
-                    />
-                  }
-                  label='Repas et Fiesta'
-                />
-              </FormGroup>
-              <TextField
-                id='allergy'
-                select
-                variant='standard'
-                label='Des allergies ?'
-                value={responseAllergy}
-                onChange={(event) => setResponseAllergy(event.target.value)}
-              >
-                <MenuItem value={"aucune"}>Aucune</MenuItem>
-                <MenuItem value={"oui"}>Oui</MenuItem>
-                <MenuItem value={"non"}>Non</MenuItem>
-              </TextField>
-              <TextField
-                id='allergies'
-                variant='standard'
-                label='Lesquelles ?'
-                value={isAllergy}
-                onChange={(event) => setIsAllergy(event.target.value)}
-              />
-              <TextField
-                id='children'
-                select
-                variant='standard'
-                label='Avec des enfants ?'
-                value={responseChildren}
-                onChange={(event) => setResponseChildren(event.target.value)}
-              >
-                <MenuItem value={"Ne sait pas"}>Ne sais pas</MenuItem>
-                <MenuItem value={"oui"}>Oui</MenuItem>
-                <MenuItem value={"non"}>Non</MenuItem>
-              </TextField>
-              {responseChildren === "oui" && (
-                <Button
-                  text={"Ajouter un enfant"}
-                  variant={"contained"}
-                  startIcon={<AddReaction />}
-                  size={"small"}
-                  style={{
-                    height: 35,
-                    backgroundColor: theme.palette.primary.main,
-                    color: theme.palette.primary.light,
+              <Grid item>
+                <TextField
+                  id='presence'
+                  select
+                  variant='standard'
+                  label='Votre présence ?'
+                  sx={{
+                    fontFamily: oswaldFontLight.fontFamily,
+                    fontWeight: oswaldFontLight.fontWeight,
+                    fontStyle: oswaldFontLight.fontStyle,
                   }}
-                  onClick={() =>
-                    setChildrenList((childrenList) => [
-                      ...childrenList,
-                      {
-                        firstName: "",
-                        lastName: "",
-                        age: "",
-                        isAllergy: false,
-                        allergies: "",
-                      },
-                    ])
-                  }
-                />
+                  value={responsePresence}
+                  onChange={(event) => setResponsePresence(event.target.value)}
+                >
+                  <MenuItem value={"Ne sais pas"}>Ne sais pas</MenuItem>
+                  <MenuItem value={"Oui"}>Oui</MenuItem>
+                  <MenuItem value={"Non"}>Non</MenuItem>
+                </TextField>
+              </Grid>
+              {responsePresence === "Oui" && (
+                <Grid item>
+                  <FormGroup>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={engagementCeremony}
+                          onChange={(event) =>
+                            setEngagementCeremony(event.target.checked)
+                          }
+                        />
+                      }
+                      label='A la cérémonie d’engagement'
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={wineReception}
+                          onChange={(event) =>
+                            setWineReception(event.target.checked)
+                          }
+                        />
+                      }
+                      label='Au vin d’honneur et Apéritif'
+                    />
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={meal}
+                          onChange={(event) => setMeal(event.target.checked)}
+                        />
+                      }
+                      label='Repas et Fiesta'
+                    />
+                  </FormGroup>
+                </Grid>
               )}
-              <Grid container direction={"column"}>
-                {childrenList &&
-                  childrenList.map((child, index) => (
-                    <Grid item key={index}>
+            </Grid>
+            {responsePresence === "Oui" && (
+              <Grid
+                className={classes.subContainer}
+                container
+                justifyContent={"space-around"}
+              >
+                <Grid item xs={12} md={4}>
+                  <TextField
+                    id='allergy'
+                    select
+                    variant='standard'
+                    label='Des allergies ?'
+                    value={responseAllergy}
+                    onChange={(event) => setResponseAllergy(event.target.value)}
+                  >
+                    <MenuItem value={"Aucune"}>Aucune</MenuItem>
+                    <MenuItem value={"Oui"}>Oui</MenuItem>
+                    <MenuItem value={"Non"}>Non</MenuItem>
+                  </TextField>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  {responseAllergy === "Oui" && (
+                    <TextField
+                      id='allergies'
+                      variant='standard'
+                      label='Lesquelles ?'
+                      value={isAllergy}
+                      onChange={(event) => setIsAllergy(event.target.value)}
+                    />
+                  )}
+                </Grid>
+              </Grid>
+            )}
+            {responsePresence === "Oui" && (
+              <Grid
+                className={classes.subContainer}
+                container
+                justifyContent={"space-around"}
+                alignItems={"center"}
+              >
+                <Grid item xs={12} md={4}>
+                  <TextField
+                    id='children'
+                    select
+                    variant='standard'
+                    label='Avec des enfants ?'
+                    value={responseChildren}
+                    onChange={(event) =>
+                      setResponseChildren(event.target.value)
+                    }
+                  >
+                    <MenuItem value={"Ne sait pas"}>Ne sais pas</MenuItem>
+                    <MenuItem value={"Oui"}>Oui</MenuItem>
+                    <MenuItem value={"Oon"}>Non</MenuItem>
+                  </TextField>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  {responseChildren === "Oui" && (
+                    <Button
+                      text={"Ajouter un enfant"}
+                      variant={"contained"}
+                      startIcon={<AddReaction />}
+                      size={"small"}
+                      style={{
+                        height: 35,
+                        backgroundColor: theme.palette.primary.main,
+                        color: theme.palette.primary.light,
+                      }}
+                      onClick={() =>
+                        setChildrenList((childrenList) => [
+                          ...childrenList,
+                          {
+                            id: Date.now(),
+                            firstName: "",
+                            lastName: "",
+                            age: "",
+                            isAllergy: "",
+                            allergies: "",
+                          },
+                        ])
+                      }
+                    />
+                  )}
+                </Grid>
+              </Grid>
+            )}
+            <Grid
+              className={classes.subContainer}
+              container
+              justifyContent={"space-evenly"}
+            >
+              {childrenList &&
+                childrenList.map((child, index) => (
+                  <div className={classes.formChild} key={index}>
+                    <Grid item xs={12}>
                       <TextField
                         id={`lastName-${index}`}
                         variant='standard'
@@ -200,6 +277,8 @@ const Formulaire = () => {
                         value={child.lastName}
                         onChange={(event) => onLastNameChange(event, index)}
                       />
+                    </Grid>
+                    <Grid item xs={12}>
                       <TextField
                         id={`firstName-${index}`}
                         variant='standard'
@@ -207,6 +286,8 @@ const Formulaire = () => {
                         value={child.firstName}
                         onChange={(event) => onFirstNameChange(event, index)}
                       />
+                    </Grid>
+                    <Grid item xs={12}>
                       <TextField
                         id={`age-${index}`}
                         variant='standard'
@@ -216,8 +297,52 @@ const Formulaire = () => {
                         onChange={(event) => onAgeChange(event, index)}
                       />
                     </Grid>
-                  ))}
-              </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        id={`childIsAllergy-${index}`}
+                        select
+                        variant='standard'
+                        label='Des allergies ?'
+                        value={child.isAllergy}
+                        onChange={(event) =>
+                          onChildIsAllergyChange(event, index)
+                        }
+                      >
+                        <MenuItem value={"Aucune"}>Aucune</MenuItem>
+                        <MenuItem value={"Oui"}>Oui</MenuItem>
+                        <MenuItem value={"Non"}>Non</MenuItem>
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12}>
+                      {child.isAllergy === "Oui" && (
+                        <TextField
+                          id={`childAllergies-${index}`}
+                          variant='standard'
+                          label='Lesquelles ?'
+                          value={child.allergies}
+                          onChange={(event) =>
+                            onChildAllergiesChange(event, index)
+                          }
+                        />
+                      )}
+                    </Grid>
+                    <Grid container justifyContent={"flex-end"}>
+                      <Button
+                        text={"Supprimer enfant"}
+                        variant={"contained"}
+                        startIcon={<AddReaction />}
+                        size={"small"}
+                        style={{
+                          height: 35,
+                          backgroundColor: theme.palette.primary.main,
+                          color: theme.palette.primary.light,
+                          marginTop: 10,
+                        }}
+                        onClick={() => onDeleteChildClick(child)}
+                      />
+                    </Grid>
+                  </div>
+                ))}
             </Grid>
           </Box>
         </Grid>
