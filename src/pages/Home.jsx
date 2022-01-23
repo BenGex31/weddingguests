3dfab962f5d3180a695022e8a072b56133af18e1
Benjamin Desole <benjamin.desole@gmail.com>
2022-01-23 10:53:27 +0100

    Update Home.jsx
    fix bug responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,7 @@ import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { AuthContext } from "../components/Auth";
 import Header from "../components/Header";
-import {
-  Typography,
-  Divider,
-  Grid,
-  Container,
-  CssBaseline,
-} from "@mui/material";
+import { Typography, Divider, Grid, Container, Stack } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { oswaldRegular as OswaldR } from "../core/theme/CustomTheme";
 import { alexBrushRegular as alexBrush } from "../core/theme/CustomTheme";
@@ -29,137 +23,126 @@ const Home = () => {
 
   return (
     <Container component='main' maxWidth='xl'>
-      <CssBaseline />
-      <Grid container justifyContent='center'>
+      <header>
         <Header />
-        <Grid mb={10}>
-          <Grid
-            container
-            direction='column'
-            justifyContent='space-around'
-            alignItems='center'
-            height={300}
-          >
-            <MainTitle title='Nous avons choisi une date' />
-            <Typography
-              sx={{
-                fontSize: 25,
-                fontFamily: OswaldR.fontFamily,
-                fontWeight: OswaldR.fontWeight,
-                fontStyle: OswaldR.fontStyle,
-                color: theme.palette.secondary.main,
-              }}
-              variant='h2'
-            >
-              Et nous comptons les jours!
-            </Typography>
-            <Grid>
-              <Divider
-                sx={{
-                  width: 300,
-                }}
-                orientation='horizontal'
-              >
-                <FavoriteIcon
-                  sx={{
-                    color: theme.palette.primary.main,
-                    width: 36,
-                    height: 31.5,
-                  }}
-                />
-              </Divider>
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            direction={{ xs: "column", md: "row" }}
-            justifyContent='space-evenly'
-            alignItems='center'
-            height={{ xs: 700, md: 150 }}
-            width={{ xs: 600, md: 700, lg: 1000 }}
-          >
-            <Cards
-              number={
-                <Countdown
-                  date={Date.now() + (new Date(2022, 4, 26) - new Date())}
-                  renderer={({ days }) => {
-                    return <span>{days}</span>;
-                  }}
-                />
-              }
-              time={"Jours"}
-            />
-            <Cards
-              number={
-                <Countdown
-                  date={Date.now() + (new Date(2022, 4, 26) - new Date())}
-                  renderer={({ hours }) => {
-                    return <span>{hours}</span>;
-                  }}
-                />
-              }
-              time={"Heures"}
-            />
-            <Cards
-              number={
-                <Countdown
-                  date={Date.now() + (new Date(2022, 4, 26) - new Date())}
-                  renderer={({ minutes }) => {
-                    return <span>{minutes}</span>;
-                  }}
-                />
-              }
-              time={"minutes"}
-            />
-            <Cards
-              number={
-                <Countdown
-                  date={Date.now() + (new Date(2022, 5, 25) - new Date())}
-                  renderer={({ seconds }) => {
-                    return <span>{seconds}</span>;
-                  }}
-                />
-              }
-              time={"Secondes"}
-            />
-          </Grid>
-          <Grid
-            container
-            direction='column'
-            height={200}
-            justifyContent='space-between'
-          >
-            <Typography
-              sx={{
-                fontSize: 38,
-                fontFamily: alexBrush.fontFamily,
-                fontWeight: alexBrush.fontWeight,
-                fontStyle: alexBrush.fontStyle,
-                color: theme.palette.primary.main,
-              }}
-              align='center'
-              variant='h3'
-            >
-              25 mai 2022
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: { xs: 52, md: 90 },
-                fontFamily: alexBrush.fontFamily,
-                fontWeight: alexBrush.fontWeight,
-                fontStyle: alexBrush.fontStyle,
-                color: theme.palette.primary.main,
-              }}
-              align='center'
-              variant='h1'
-            >
-              L'heureux couple
-            </Typography>
-          </Grid>
-        </Grid>
-        <WeddingAvatar />
-        <ImageResponse />
+      </header>
+      <Grid mb={5} container justifyContent={"center"}>
+        <MainTitle title='Nous avons choisi une date' />
       </Grid>
+      <Grid mb={5} container justifyContent={"center"}>
+        <Typography
+          sx={{
+            fontSize: 25,
+            fontFamily: OswaldR.fontFamily,
+            fontWeight: OswaldR.fontWeight,
+            fontStyle: OswaldR.fontStyle,
+            color: theme.palette.secondary.main,
+          }}
+          variant='h2'
+        >
+          Et nous comptons les jours!
+        </Typography>
+      </Grid>
+      <Grid mb={5} container justifyContent={"center"} alignItems={"center"}>
+        <Divider
+          sx={{
+            width: 300,
+          }}
+          orientation='horizontal'
+        >
+          <FavoriteIcon
+            sx={{
+              color: theme.palette.primary.main,
+              width: 36,
+              height: 31.5,
+            }}
+          />
+        </Divider>
+      </Grid>
+      <Stack
+        mb={5}
+        spacing={{ xs: 3 }}
+        justifyContent={"space-evenly"}
+        alignItems={"center"}
+        direction={{ xs: "column", md: "row" }}
+      >
+        <Cards
+          number={
+            <Countdown
+              date={Date.now() + (new Date(2022, 4, 26) - new Date())}
+              renderer={({ days }) => {
+                return <span>{days}</span>;
+              }}
+            />
+          }
+          time={"Jours"}
+        />
+        <Cards
+          number={
+            <Countdown
+              date={Date.now() + (new Date(2022, 4, 26) - new Date())}
+              renderer={({ hours }) => {
+                return <span>{hours}</span>;
+              }}
+            />
+          }
+          time={"Heures"}
+        />
+        <Cards
+          number={
+            <Countdown
+              date={Date.now() + (new Date(2022, 4, 26) - new Date())}
+              renderer={({ minutes }) => {
+                return <span>{minutes}</span>;
+              }}
+            />
+          }
+          time={"minutes"}
+        />
+        <Cards
+          number={
+            <Countdown
+              date={Date.now() + (new Date(2022, 5, 25) - new Date())}
+              renderer={({ seconds }) => {
+                return <span>{seconds}</span>;
+              }}
+            />
+          }
+          time={"Secondes"}
+        />
+      </Stack>
+      <Grid mb={5} container justifyContent={"center"}>
+        <Typography
+          sx={{
+            fontSize: 38,
+            fontFamily: alexBrush.fontFamily,
+            fontWeight: alexBrush.fontWeight,
+            fontStyle: alexBrush.fontStyle,
+            color: theme.palette.primary.main,
+          }}
+          align='center'
+          variant='h3'
+        >
+          25 mai 2022
+        </Typography>
+      </Grid>
+      <Grid mb={5} justifyContent={"center"}>
+        <Typography
+          sx={{
+            fontSize: { xs: 52, md: 90 },
+            fontFamily: alexBrush.fontFamily,
+            fontWeight: alexBrush.fontWeight,
+            fontStyle: alexBrush.fontStyle,
+            color: theme.palette.primary.main,
+          }}
+          align='center'
+          variant='h1'
+        >
+          L'heureux couple
+        </Typography>
+      </Grid>
+      <WeddingAvatar />
+      <ImageResponse />
     </Container>
   );
 };
