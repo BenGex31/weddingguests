d6d3ec249234462b403676fe3f6a8c9bfef6084d
Benjamin Desole <benjamin.desole@gmail.com>
2022-01-28 21:46:48 +0100

    Update MainTitle.jsx
    refactor

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import theme from "../core/theme/MuiTheme";
 import { alexBrushRegular as alexBrush } from "../core/theme/CustomTheme";
 
 const MainTitle = ({ title }) => {
   return (
-    <Grid item justifyContent='center'>
-      <Typography
-        sx={{
-          fontSize: { md: 66, xs: 40 },
-          fontFamily: alexBrush.fontFamily,
-          fontWeight: alexBrush.fontWeight,
-          fontStyle: alexBrush.fontStyle,
-          color: theme.palette.primary.main,
-        }}
-        align='center'
-        variant='h1'
-      >
-        {title}
-      </Typography>
-    </Grid>
+    <Typography
+      sx={{
+        fontSize: { md: 66, xs: 40 },
+        fontFamily: alexBrush.fontFamily,
+        fontWeight: alexBrush.fontWeight,
+        fontStyle: alexBrush.fontStyle,
+        color: theme.palette.primary.main,
+      }}
+      align='center'
+      variant='h1'
+    >
+      {title}
+    </Typography>
   );
 };
 
