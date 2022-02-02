d63545800c7e7eec8d9fad7834f59c21f9a34d92
Benjamin Desole <benjamin.desole@gmail.com>
2022-02-02 22:49:03 +0100

    Update Profil.jsx
    define container and first grid to display

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,11 +1,34 @@
+import { Box, Container, Grid } from "@mui/material";
 import React from "react";
 import Header from "../components/Header";
+import theme from "../core/theme/MuiTheme";
 
 const Profil = () => {
   return (
-    <header>
-      <Header />
-    </header>
+    <Container component='main' maxWidth='xl'>
+      <header>
+        <Header />
+      </header>
+      <Grid
+        mb={2}
+        container
+        sx={{
+          boxShadow: "0px 0px 20px" + theme.palette.secondary.main,
+          borderRadius: 5,
+          height: 731,
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            height: 222,
+            backgroundColor: theme.palette.primary.main,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+          }}
+        />
+      </Grid>
+    </Container>
   );
 };
 
