12c47d63df63923ab8683af75cfbc64415ee81ab
Benjamin Desole <benjamin.desole@gmail.com>
2022-02-05 11:39:39 +0100

    Update NavTab.jsx
    remove unused import

--- src/components/NavTab.jsx
+++ src/components/NavTab.jsx
@@ -5,7 +5,6 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { oswaldRegular } from "../core/theme/CustomTheme";
-import theme from "../core/theme/MuiTheme";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
