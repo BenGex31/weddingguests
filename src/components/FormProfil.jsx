69501baab6629d968ae269fc5e12206ba439fb1a
Benjamin Desole <benjamin.desole@gmail.com>
2022-02-05 22:53:48 +0100

    Update FormProfil.jsx
    implement Dialog to update info user
    implement SnackBar to inform user
    manage Texfield errors with helper text

--- src/components/FormProfil.jsx
+++ src/components/FormProfil.jsx
@@ -2,20 +2,44 @@ import React from "react";
 import { AuthContext } from "./Auth";
 import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
 import firebaseConfig from "../config/firebase";
-import { Button, Grid, TextField } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Stack,
+  TextField,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import { IconButton } from "@mui/material";
 import theme from "../core/theme/MuiTheme";
+import { Close } from "@mui/icons-material";
 
 const FormProfil = () => {
   const { currentUser } = React.useContext(AuthContext);
   const [guest, setGuest] = React.useState(null);
   const [user, setUser] = React.useState({
-    phoneNumber: guest !== null && guest.phoneNumber,
+    fullName: "",
+    phoneNumber: "",
   });
+  const [openDialog, setopenDialog] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState({
+    phoneNumber: "",
+    validPhoneNumber: true,
+    fullName: "",
+    validFullName: true,
+  });
+  const [openSnackBar, setOpenSnackBar] = React.useState(false);
+  const [messageSnackBar, setMessageSnackBar] = React.useState("");
+  const [severitySnackBar, setSeveritySnackBar] = React.useState("success");
 
   React.useEffect(() => {
     getDocUser();
     // eslint-disable-next-line
-  }, [user.phoneNumber]);
+  }, []);
 
   const getDocUser = async () => {
     const docRef = doc(getFirestore(firebaseConfig), "guests", currentUser.uid);
@@ -39,10 +63,79 @@ const FormProfil = () => {
         currentUser.uid
       );
       await updateDoc(guestRef, {
-        phoneNumber: user.phoneNumber,
+        phoneNumber:
+          user.phoneNumber !== "" ? user.phoneNumber : guest.phoneNumber,
+        name: user.fullName !== "" ? user.fullName : guest.name,
       });
-    } catch (error) {}
+      getDocUser();
+      if (user.phoneNumber !== "" && user.fullName !== "") {
+        setErrorMessage({
+          ...errorMessage,
+          phoneNumber: "",
+          fullName: "",
+          validFullName: true,
+          validPhoneNumber: true,
+        });
+        setOpenSnackBar(true);
+        setMessageSnackBar("Informations mises à jours");
+        setSeveritySnackBar("success");
+      } else {
+        setErrorMessage({
+          ...errorMessage,
+          phoneNumber: "Renseigner un numéro de téléphone",
+          fullName: "Renseignez un prénom et un nom",
+          validFullName: false,
+          validPhoneNumber: false,
+        });
+      }
+      if (user.phoneNumber === "" && user.fullName !== "") {
+        setErrorMessage({
+          ...errorMessage,
+          phoneNumber: "Renseigner un numéro de téléphone",
+          fullName: "",
+          validFullName: true,
+          validPhoneNumber: false,
+        });
+      }
+      if (user.phoneNumber !== "" && user.fullName === "") {
+        setErrorMessage({
+          ...errorMessage,
+          phoneNumber: "",
+          fullName: "Renseignez un prénom et un nom",
+          validFullName: false,
+          validPhoneNumber: true,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      setMessageSnackBar("Erreur mise à jour");
+      setSeveritySnackBar("success");
+    }
   };
+
+  const handleSnackBarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenSnackBar(false);
+  };
+
+  const actionSnackBar = (
+    <React.Fragment>
+      <Button color='secondary' size='small' onClick={handleSnackBarClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size='small'
+        aria-label='close'
+        color='inherit'
+        onClick={handleSnackBarClose}
+      >
+        <Close fontSize='small' />
+      </IconButton>
+    </React.Fragment>
+  );
 
   return (
     <Grid container>
@@ -99,10 +192,89 @@ const FormProfil = () => {
             textTransform: "none",
             marginTop: 5,
           }}
+          onClick={() => setopenDialog(true)}
         >
           Mettre à jour
         </Button>
       </Grid>
+      <Dialog
+        open={openDialog}
+        onClose={() => setopenDialog(false)}
+        maxWidth='md'
+        fullWidth
+      >
+        <DialogTitle>Mettre à jour votre profil</DialogTitle>
+        <DialogContent>
+          <Stack direction='row' justifyContent='space-around'>
+            <TextField
+              label='Prénom Nom'
+              value={user.fullName}
+              variant='standard'
+              onChange={(event) =>
+                setUser({ ...user, fullName: event.target.value })
+              }
+              helperText={
+                errorMessage.validFullName === false
+                  ? errorMessage.fullName
+                  : ""
+              }
+              error={!errorMessage.validFullName}
+            />
+            <TextField
+              label='Téléphone mobile'
+              value={user.phoneNumber}
+              variant='standard'
+              onChange={(event) =>
+                setUser({ ...user, phoneNumber: event.target.value })
+              }
+              helperText={
+                errorMessage.validPhoneNumber === false
+                  ? errorMessage.phoneNumber
+                  : ""
+              }
+              error={!errorMessage.validPhoneNumber}
+            />
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant='contained'
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              textTransform: "none",
+              marginTop: 5,
+            }}
+            onClick={() => setopenDialog(false)}
+          >
+            Fermer
+          </Button>
+          <Button
+            variant='contained'
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              textTransform: "none",
+              marginTop: 5,
+            }}
+            onClick={setUserDatas}
+          >
+            Envoyer
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={openSnackBar}
+        autoHideDuration={4000}
+        onClose={handleSnackBarClose}
+        action={actionSnackBar}
+      >
+        <Alert
+          onClose={handleSnackBarClose}
+          severity={severitySnackBar}
+          sx={{ width: "100%" }}
+        >
+          {messageSnackBar}
+        </Alert>
+      </Snackbar>
     </Grid>
   );
 };
