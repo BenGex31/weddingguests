d7085d28bac31c8086be5325e2884c151646ff00
Benjamin Desole <benjamin.desole@gmail.com>
2022-02-13 01:37:12 +0100

    Update Guests.jsx

--- src/pages/Guests.jsx
+++ src/pages/Guests.jsx
@@ -23,6 +23,7 @@ import { Mail } from "@material-ui/icons";
 import { makeStyles } from "@mui/styles";
 import MainTitle from "../components/MainTitle";
 import SubTitle from "../components/SubTitle";
+import { Face } from "@mui/icons-material";
 
 const useStyles = makeStyles({
   mailIcon: {
@@ -33,25 +34,45 @@ const useStyles = makeStyles({
 const Guests = () => {
   const classes = useStyles();
   const [guests, setGuests] = React.useState([]);
+  const [children, setChildren] = React.useState([]);
 
   React.useEffect(() => {
-    getCollection();
+    getGuestsCollection();
+    getChildrenCollection();
   }, []);
 
-  const getCollection = async () => {
-    const querySnapshot = await getDocs(
+  const getGuestsCollection = async () => {
+    const queryGuestSnapshot = await getDocs(
       collection(getFirestore(firebaseConfig), "guests")
     );
 
     let array = [];
 
-    querySnapshot.forEach((doc) => {
+    queryGuestSnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
       console.log(doc.id, " => ", doc.data());
       array.push(doc.data());
     });
     setGuests(array);
   };
+
+  const getChildrenCollection = async () => {
+    const queryChildrenSnapshot = await getDocs(
+      collection(getFirestore(firebaseConfig), "childrenGuests")
+    );
+
+    let array = [];
+
+    queryChildrenSnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(queryChildrenSnapshot);
+      console.log(doc.id, " => ", doc.data());
+      array.push(doc.data().childrenList);
+    });
+    setChildren(array);
+  };
+  console.log(children.concat().flat());
+
   return (
     <Container component='main' maxWidth='xl'>
       <header>
@@ -102,6 +123,7 @@ const Guests = () => {
                 height: 284,
                 backgroundColor: theme.palette.primary.main,
                 boxShadow: "0px 0px 20px" + theme.palette.secondary.main,
+                marginBottom: 5,
               }}
             >
               <CardHeader
@@ -111,7 +133,7 @@ const Guests = () => {
                       width: 80,
                       height: 80,
                       backgroundColor: theme.palette.primary.light,
-                      color: "#000",
+                      color: theme.palette.secondary.main,
                     }}
                     alt={guest.name}
                     src={guest.photo}
@@ -138,6 +160,7 @@ const Guests = () => {
                       justifyContent={"flex-start"}
                       direction={"row"}
                       spacing={1}
+                      sx={{ paddingRight: 3 }}
                     >
                       <Mail className={classes.mailIcon} fontSize='large' />
                       <Typography
@@ -177,7 +200,7 @@ const Guests = () => {
                       } ans`}
                     </Typography>
                   </Stack>
-                  <Stack>
+                  <Stack alignItems='flex-end'>
                     <FormGroup>
                       <FormControlLabel
                         disabled
@@ -207,6 +230,90 @@ const Guests = () => {
                         label={"Repas & Fiesta"}
                       />
                     </FormGroup>
+                  </Stack>
+                </Stack>
+              </CardContent>
+            </Card>
+          ))}
+      </Grid>
+      <MainTitle
+        title={`Les enfants présents (${children.concat().flat().length})`}
+      />
+      <Grid container mb={5} mt={2} justifyContent={"space-around"}>
+        {children
+          .concat()
+          .flat()
+          .map((child, index) => (
+            <Card
+              key={index}
+              sx={{
+                width: 441,
+                height: 284,
+                backgroundColor: theme.palette.primary.main,
+                boxShadow: "0px 0px 20px" + theme.palette.secondary.main,
+                marginBottom: 5,
+              }}
+            >
+              <CardHeader
+                avatar={
+                  <Face
+                    sx={{
+                      width: 80,
+                      height: 80,
+                      backgroundColor: theme.palette.primary.light,
+                      color: theme.palette.secondary.main,
+                      borderRadius: 20,
+                    }}
+                  />
+                }
+                title={
+                  <Typography
+                    sx={{
+                      fontSize: 18,
+                      color: theme.palette.primary.light,
+                      fontFamily: OswaldR.fontFamily,
+                      fontWeight: OswaldR.fontWeight,
+                      fontStyle: OswaldR.fontStyle,
+                    }}
+                  >
+                    {`${child.firstName} ${child.lastName}`}
+                  </Typography>
+                }
+              />
+              <CardContent>
+                <Stack direction={"row"} justifyContent={"center"}>
+                  <Stack alignItems='center'>
+                    <Stack
+                      justifyContent={"flex-start"}
+                      direction={"row"}
+                      spacing={1}
+                    >
+                      <Mail className={classes.mailIcon} fontSize='large' />
+                      <Typography
+                        sx={{
+                          fontSize: 30,
+                          fontFamily: alexBrush.fontFamily,
+                          fontWeight: alexBrush.fontWeight,
+                          fontStyle: alexBrush.fontStyle,
+                          color: theme.palette.primary.light,
+                        }}
+                      >
+                        Invité
+                      </Typography>
+                    </Stack>
+                    <Typography
+                      sx={{
+                        fontSize: 18,
+                        fontFamily: oswaldLight.fontFamily,
+                        fontWeight: oswaldLight.fontWeight,
+                        fontStyle: oswaldLight.fontStyle,
+                        color: theme.palette.primary.light,
+                      }}
+                    >
+                      {`${child.age >= 18 ? "Adulte" : "Enfant"} ${
+                        child.age
+                      } ans`}
+                    </Typography>
                   </Stack>
                 </Stack>
               </CardContent>
